b970fb1e772f56487dc9f1cdc4a074c9062a61a7
Frederik Emmer <frederik.emmer@icloud.com>
2023-12-09 10:16:12 +0100

    updated eval

--- evaluation.py
+++ evaluation.py
@@ -331,23 +331,23 @@ def test_nn(*args, mean_from=10):
         
     if "learn" in args:
         # Test neural network mit verschiedenen "learning_rate_init" Werten
-        neural_network_scores = test_neural_network(learning_rate_init=[0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100], mean_from=mean_from)
+        neural_network_scores = test_neural_network(learning_rate_init=[0.0001, 0.001, 0.01, 0.1, 1, 10, 100], mean_from=mean_from)
         plot_accuracy_scores(neural_network_scores, xlabel="Learning Rate", ylabel="Genauigkeit",
                             title="Genauigkeit des Neural Network mit verschiedenen Learning Rate-Werten",
                             savefig=True, filename="neural_network_learning_rate")    
         
 
 
-#test_linear_regression("fit", "copy", mean_from=10)
+#test_linear_regression("fit", "copy")
 
-#test_nn("layouts", "neurons", "layers", "activation", "solver", "alpha", mean_from=10)
-test_nn("learn", mean_from=10)
-#test_nn("layouts", mean_from=10)
-#test_nn("alpha", mean_from=10)
-#test_nn("neurons", mean_from=10)
-#test_nn("layers", mean_from=10)
+#test_nn("layouts", "neurons", "layers", "activation", "solver", "alpha")
+test_nn("learn")
+#test_nn("layouts")
+#test_nn("alpha")
+#test_nn("neurons")
+#test_nn("layers")
 
-#test_svm("c", "kernel", "gamma", mean_from=10)
+#test_svm("c", "kernel", "gamma")
 
-#test_knn("p", mean_from=10)
-#test_knn("n", "weights", "algo", "leaf", "p", mean_from=10)
+#test_knn("p")
+#test_knn("n", "weights", "algo", "leaf", "p")
